d7bb2a5e1e3b6048ee58c9fe6a97ca2b6c1a1bac
Beauty kumari <kbeauty193@gmail.com>
2024-09-30 18:00:48 +0530

    commit

--- problems/p2.java
+++ problems/p2.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+class p2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int product = number * i;
+            System.out.println(product);
+
+        }
+
+    }
+}
