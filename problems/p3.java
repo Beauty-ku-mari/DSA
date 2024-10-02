d8a2629eeeb924b3fd0d5a51bc8c9a2e4ca526de
Beauty kumari <kbeauty193@gmail.com>
2024-10-02 22:27:09 +0530

    'commit'

---
+++ problems/p3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class p3 {
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+switch(a){
+case 1: System.out.println("january");
+break;
+case 2: System.out.println("Feburary");
+break;
+case 3: System.out.println("march");
+break;
+case 4: System.out.println("april");
+break;
+case 5: System.out.println("may");
+break;
+case 6: System.out.println("june");
+break;
+case 7: System.out.println("july");
+break;
+case 8: System.out.println("August");
+break;
+case 9: System.out.println("september");
+break;
+case 10: System.out.println("october");
+break;
+case 11: System.out.println("november");
+break;
+case 12: System.out.println("december");
+break;
+default: System.out.println("none");
+break;
+}
+}
+}
