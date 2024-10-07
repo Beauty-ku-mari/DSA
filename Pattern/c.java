4d7469143357b3de04eca77fca80ca5d642ed8b0
Beauty kumari <kbeauty193@gmail.com>
2024-10-07 18:03:36 +0530

    commit

---
+++ Pattern/c.java
@@ -0,0 +1,14 @@
+public class c {
+    public static void main(String[] args) {
+        int m = 4;
+        int n = 5;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+    }
+}
